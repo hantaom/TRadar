a3b61517a1b223f8805e0e758a735c5f82312d31
hantaom <hantaom@gmail.com>
2018-08-19 14:26:00 -0700

    Initial Commit

---
+++ TRadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRadar
+//
+//  Created by Henry Jones on 2018-08-19.
+//  Copyright © 2018 Hantao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
